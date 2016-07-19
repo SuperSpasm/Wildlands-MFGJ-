342cee58000cc7f54d4568876f741d1566d2bca4
amanarya0 <aman.arya00@gmail.com>
2016-07-19 22:27:24 +0530

    Added breaking branches

---
+++ Assets/Scripts/BreakBranch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakBranch : MonoBehaviour {
+
+	public float timeToFall;
+	private Rigidbody2D rb;
+	private float elapsedTime;
+
+	void Awake () {
+		elapsedTime = 0.0f;
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime >= timeToFall) {
+			rb.gravityScale = 1.0f;
+			rb.isKinematic = false;
+			this.enabled = false;
+			return;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager{
+
+	private static GameManager instance = new GameManager ();
+
+	private GameManager()
+	{
+		
+	}
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if(instance == null)
+			{
+				instance = new GameManager();
+			}
+			return instance;
+		}
+	}
+}
