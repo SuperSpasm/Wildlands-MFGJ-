eb5d73064a966e32b06df385633d31a0e80b8d8f
Jonathan Levin <JonathanWuzHere@gmail.com>
2016-07-22 13:34:43 +0300

    Add breaking branches sprites / prefabs, import some more assets

---
+++ Assets/ChangeBreakTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeBreakTime : MonoBehaviour {
+    public float breakTime = 1.5f;
+
+    private BreakBranch branchScript;
+
+    void OnValidate()
+    {
+        if (!branchScript)
+            branchScript = GetComponentInChildren<BreakBranch>();
+
+        branchScript.timeToFall = breakTime;
+    }
+}
